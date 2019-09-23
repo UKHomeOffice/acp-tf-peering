fc4a84ff7da698eb37d36a7b62f243f553761cff
adekunle ibitayo <adekunle.ibitayo@appvia.io>
2019-09-23 13:54:35 +0100

    upgrading to terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
 output "peering_id" {
   description = "The peering connection id for the connection"
-  value       = "${aws_vpc_peering_connection.request.id}"
+  value       = aws_vpc_peering_connection.request.id
 }
 
 output "vpc_source" {
   description = "The source vpc id of the peering connection"
-  value       = "${var.vpc_source["vpc_id"]}"
+  value       = var.vpc_source["vpc_id"]
 }
 
 output "vpc_dest" {
   description = "The destination vpc id of the peering connection"
-  value       = "${var.vpc_dest["vpc_id"]}"
+  value       = var.vpc_dest["vpc_id"]
 }
+
